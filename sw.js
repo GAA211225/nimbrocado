271fb4a8006435e9904a49e09273c9b82a60bebc
GAA211225 <halo21122505@gmail.com>
2026-07-12 20:00:56 -0600

    Import Embobate app

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'emboobate-cache-v23';
+// Service worker propio de Embobate (separado de dieta/rutinas desde 2026-07-07).
+const ASSETS = [
+  './embobate.html',
+  './manifest.json',
+  './logo.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
